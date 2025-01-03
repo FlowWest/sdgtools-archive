c2cd3eef6c11d7181cde6b3839c53cce297e2cd9
ergz <erodriguez@flowwest.com>
2025-01-03 12:28:43 -0800

    removes class and uses updated version of read_all*

--- src/sdgtools/dss_reader/__init__.py
+++ src/sdgtools/dss_reader/__init__.py
@@ -10,100 +10,6 @@ from typing import Any, Dict, List
 from pandas.core.series import validate_bool_kwarg
 
 
-@dataclass
-class SDGScenario:
-    path: str
-    name: str
-
-    def __locate_files(self):
-        output_files = os.listdir("output")
-        echo_file = [
-            f"output/{f}" for f in output_files if "echo" in f and self.name in f
-        ]
-        dss_files = [
-            f"output/{f}" for f in output_files if "dss" in f and self.name in f
-        ]
-
-        return {"echo": echo_file, "dss": dss_files}
-
-    def __read_echo_file(self, filepath: str):
-        with open(filepath, "r") as f:
-            lines = f.readlines()
-
-        start_index = next(
-            (i for i, line in enumerate(lines) if "GATE_WEIR_DEVICE" in line), None
-        )
-        end_index = next(
-            (i for i, line in enumerate(lines) if "END" in line and i > start_index),
-            None,
-        )
-
-        if start_index is not None and end_index is not None:
-            data_rows = lines[start_index + 1 : end_index]
-            col_names = [
-                "GATE_NAME",
-                "DEVICE",
-                "NDUPLICATE",
-                "WIDTH",
-                "ELEV",
-                "HEIGHT",
-                "CF_FROM_NODE",
-                "CF_TO_NODE",
-                "DEFAULT_OP",
-            ]
-
-            data = [line.split(maxsplit=len(col_names) - 1) for line in data_rows[1:]]
-            df = pd.DataFrame(data, columns=col_names)
-            df = df[df["DEVICE"] == "fish_passage"]
-            return df
-
-    def __post_init__(self):
-        self.files = self.__locate_files()
-        self.echo = self.__read_echo_file(self.files.get("echo")[0])
-
-    def read_dss(
-        self,
-        dss: HecDss,
-        part_names: Dict[str, str] | None = None,
-        concat: bool = False,
-    ) -> pd.DataFrame | Dict[str, pd.DataFrame]:
-        out = {}
-        cat = dss.get_catalog()
-        paths = list(cat.recordTypeDict.keys())
-        for i, path in enumerate(paths):
-            path_data = dss.get(path)
-            out[path] = pd.DataFrame(
-                {
-                    "datetime": path_data.get_dates(),  # type: ignore
-                    "value": path_data.get_values(),  # type: ignore
-                    "unit": path_data.units,  # type: ignore
-                }
-            )
-            if part_names is not None:
-                if "A" in part_names:
-                    out[path][part_names.get("A")] = cat.items[i].A
-                if "B" in part_names:
-                    out[path][part_names.get("B")] = cat.items[i].B
-                if "C" in part_names:
-                    out[path][part_names.get("C")] = cat.items[i].C
-                if "D" in part_names:
-                    out[path][part_names.get("D")] = cat.items[i].D
-                if "E" in part_names:
-                    out[path][part_names.get("E")] = cat.items[i].E
-                if "F" in part_names:
-                    out[path][part_names.get("F")] = cat.items[i].F
-
-        if concat:
-            out = pd.concat(out.values())
-        return out
-
-
-def read_scenario(path):
-    hydo_dss = ""
-    sdg_dss = ""
-    echo_file = ""
-
-
 def read_echo_file(filepath: str):
     with open(filepath, "r") as f:
         lines = f.readlines()
@@ -171,7 +77,6 @@ def get_all_data_from_dsm2_dss(
         return pd.DataFrame()
 
     for path in paths:
-        print("made it to here---------")
         path_data = dss.get(path.get("path"))  # type: ignore
         datetimes_for_data = path_data.get_dates()  # type: ignore
         out[path.get("path")] = pd.DataFrame(
@@ -247,8 +152,8 @@ def read_scenario_dir(
     try:
         for match in matches:
             data[str(match[0])] = {
-                "hydro": read_hydro_dss(str(match[0])),
-                "sdg": read_gates_dss(str(match[1])),
+                "hydro": get_all_data_from_dsm2_dss(HecDss(str(match[0]))),
+                "sdg": get_all_data_from_dsm2_dss(HecDss(str(match[1]))),
             }
 
     except Exception as e:
