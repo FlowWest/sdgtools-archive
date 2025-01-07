f9178cebae362de827a94c2a6cb94e1b75a3907d
ergz <erodriguez@flowwest.com>
2025-01-07 14:35:38 -0800

    adds support for reading echo files at arbitrary positions

--- src/sdgtools/echo/__init__.py
+++ src/sdgtools/echo/__init__.py
@@ -1,16 +1,34 @@
 from pathlib import Path
+from typing import List
+import pandas as pd
 
 
-def read_echo(file: Path):
+def read_echo_file(file: Path, table_names: List[str] | str):
     """
-    Read in echo file and return set of data configs used for post-proessing
+    Read in echo file and return set of data configs used for post-processing
     """
-    line_idx = {"gate_weir_device": [-1, -1]}
+    if type(table_names) == str:
+        table_names = [table_names]
 
-    # find line numbers for all key tables in the echo file
+    out = {}
+
     with open(file, "r") as f:
-        for start_num, line in enumerate(f, start=1):
-            if line.strip().lower() in line_idx:
-                for end_num, next_line in enumerate(f, start=start_num):
-                    if next_line.strip().lower() == "end":
-                        line_idx[line.lower()] = [start_num, end_num]
+        all_lines = f.readlines()
+
+    for start_num, line in enumerate(all_lines, start=1):
+        key = line.strip().lower()
+        if key in table_names:
+            for end_num, next_line in enumerate(
+                all_lines[start_num:], start=start_num + 1
+            ):
+                if next_line.strip().lower() == "end":
+                    out[key] = (start_num + 1, end_num - 1)
+                    break
+
+    data = {}
+    for k, v in out.items():
+        raw_rows = [r.split() for r in all_lines[v[0] - 1 : v[1]]]
+        rows = [[r.strip() for r in d] for d in raw_rows]
+        data[k] = pd.DataFrame(rows[1:], columns=rows[0])
+
+    return data
